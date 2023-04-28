2c8fdefe539a926b3b6964485bdc29c62b7f3af8
Amukh1 <85003861+amukh1@users.noreply.github.com>
2023-04-28 10:47:02 -0400

    Looping through arrays

--- arrayTest.asm
+++ arrayTest.asm
@@ -1,52 +1,54 @@
 section .bss
-array: resb 4
+array: resb 16
 size: resb 4
-v1: resb 4
-v2: resb 4
-v3: resb 4
+index: resb 4
+sizes: resb 4
 
 section .text
 
 global _start
 
+log:
+pop edi 
+; pop edx
+pop ecx
+mov edx, [sizes + ecx*4]
+mov ecx, [array + ecx*4]
+mov ebx, 1 
+mov eax, 4 
+int 0x80 
+push edi
+ret
+
 _start:
-    mov DWORD [array + 0*4], x
-    mov DWORD [array + 1*4], y
-    mov DWORD [array + 2*4], msg
-    mov DWORD [array + 3*4], x
+ 
+    mov WORD [index], 0
+    mov WORD [size], 3
+    mov eax, x
+    mov [array + 0*4], eax
+    mov eax, y
+    mov [array + 1*4], eax
+    mov eax, msg
+    mov [array + 2*4], eax
+    mov eax, x
+    mov [array + 3*4], eax
     
-    mov eax, [array + 0*4]
-    mov ecx, eax
-    mov edx, 1
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
+    mov eax, 1
+    mov [sizes + 0*4], eax
+    mov eax, 1
+    mov [sizes + 1*4], eax
+    mov eax, 8
+    mov [sizes + 2*4], eax
+    mov eax, 1
+    mov [sizes + 3*4], eax
     
-    mov ecx, NEWLINE
-    mov edx, 1
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
+    re:
+    mov eax, [index]
+    push eax
+    call log
     
-    mov eax, [array + 1*4]
-    mov ecx, eax
-    mov edx, 1
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
-    
-    mov ecx, NEWLINE
-    mov edx, 1
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
-    
-    mov eax, [array + 2*4]
-    mov ecx, eax
-    mov edx, 8
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
+    mov eax, 1
+    add [index], eax
     
     mov ecx, NEWLINE
     mov edx, 1
@@ -54,16 +56,63 @@ _start:
     mov eax, 4
     int 0x80
     
-    mov eax, [array + 3*4]
-    mov ecx, eax
-    mov edx, 1
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
-
-
+    mov eax, [size]
+    sub eax, [index]
+    jnz re
+    
     mov     eax, 1
     int     0x80
+    
+    ; mov eax, [index]
+    ; mov eax, [array + eax*4]
+    ; mov ecx, eax
+    ; mov edx, 1
+    ; mov ebx, 1
+    ; mov eax, 4
+    ; int 0x80
+    
+    ; mov eax, 1
+    ; add [index], eax
+    
+    
+    ; mov ecx, NEWLINE
+    ; mov edx, 1
+    ; mov ebx, 1
+    ; mov eax, 4
+    ; int 0x80
+    
+    ; mov eax, [index]
+    ; mov eax, [array + eax*4]
+    ; mov ecx, eax
+    ; mov edx, 1
+    ; mov ebx, 1
+    ; mov eax, 4
+    ; int 0x80
+    
+    ; mov eax, 1
+    ; add [index], eax
+    
+    
+    ; mov ecx, NEWLINE
+    ; mov edx, 1
+    ; mov ebx, 1
+    ; mov eax, 4
+    ; int 0x80
+    
+    ; mov eax, [index]
+    ; mov eax, [array + eax*4]
+    ; mov ecx, eax
+    ; mov edx, 8
+    ; mov ebx, 1
+    ; mov eax, 4
+    ; int 0x80
+    
+    ; mov ecx, NEWLINE
+    ; mov edx, 1
+    ; mov ebx, 1
+    ; mov eax, 4
+    ; int 0x80
+
 
 section .data
     x db '5'
